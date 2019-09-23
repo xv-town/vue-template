10ede55d59597f6b26ebc50866964c4d572c546f
xiaoYown <qzy09101018@sina.com>
2019-09-24 01:22:27 +0800

    fix: css build error, source-map

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const VueSkeletonWebpackPlugin = require('vue-skeleton-webpack-plugin');
-const AssetsCDNPWebpackPlugin = require('assets-cdn-webpack-plugin');
+const AssetsCDNWebpackPlugin = require('assets-cdn-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const entries = require('./entries');
 const APP_CONFIG = require('../app.config');
@@ -12,7 +12,7 @@ const utils = require('./utils');
 // 开辟一个线程池
 // 拿到系统CPU的最大核数，happypack 将编译工作灌满所有线程
 // const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
-const IS_DEV = process.env.NODE_ENV !== 'development';
+const IS_DEV = process.env.NODE_ENV === 'development';
 
 module.exports = {
   entry: entries,
@@ -29,7 +29,7 @@ module.exports = {
             ] : []
           ),
           {
-            loader: !IS_DEV ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+            loader: IS_DEV ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
             options: {
               sourceMap: true,
               hmr: IS_DEV,
@@ -92,7 +92,7 @@ module.exports = {
   plugins: [
     new ProgressBarPlugin(),
     new VueLoaderPlugin(),
-    new AssetsCDNPWebpackPlugin(APP_CONFIG.injectAssets),
+    new AssetsCDNWebpackPlugin(APP_CONFIG.injectAssets),
     new VueSkeletonWebpackPlugin({
       webpackConfig: {
         entry: APP_CONFIG.skeletons.entry
@@ -107,7 +107,7 @@ module.exports = {
       '@': path.resolve(__dirname, '../src'),
       'vue$': 'vue/dist/vue.esm.js',
     },
-    extensions: ['.js', '.jsx', '.vue']
+    extensions: ['.js', '.vue']
   },
   externals: APP_CONFIG.externals
 };
